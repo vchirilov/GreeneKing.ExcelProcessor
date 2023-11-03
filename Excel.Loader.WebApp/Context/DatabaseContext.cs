485171f0201496815fa7d29c91edef148d486a21
Veaceslav Chirilov <veaceslav.chirilov@amdaris.com>
2023-11-03 18:09:08 +0200

    Added a new project Excel.Loader.WebApp

---
+++ Excel.Loader.WebApp/Context/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Excel.Loader.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Excel.Loader.WebApp.Context
+{
+    public class DatabaseContext: DbContext
+    {
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = string.Format(@"Data Source=51.12.52.30;Initial Catalog=Students;Persist Security Info=True;User ID=sa;Password=spartak_1");
+            optionsBuilder.UseSqlServer(connectionString);
+        }      
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
---
+++ Excel.Loader.WebApp/Services/IStudentService.cs
@@ -0,0 +1,10 @@
+﻿using Excel.Loader.WebApp.Models;
+
+namespace Excel.Loader.WebApp.Services
+{
+    public interface IStudentService
+    {
+        List<Student> GetStudents();
+        List<Student> SaveStudents(List<Student> students);
+    }
+}
