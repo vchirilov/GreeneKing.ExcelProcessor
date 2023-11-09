56eb2339d3ac890d1b9735449b49f9346441f684
Veaceslav Chirilov <veaceslav.chirilov@amdaris.com>
2023-11-09 15:38:09 +0200

    Removed 2 projects as no more needed.

--- Presentation/Models/FileUploadModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Presentation.Models
-{
-    public class FileUploadModel
-    {
-        public string PackageName { get; set; } = string.Empty;
-        public List<IFormFile> UploadedFiles { get; set; } = new List<IFormFile>();
-
-    }
-}
--- Presentation/Services/IExcelProcessorService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Presentation.Services
-{
-    public interface IExcelProcessorService
-    {
-        Task PrcessFile(Stream xlsFile);
-    }
-}
