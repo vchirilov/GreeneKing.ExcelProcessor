cf687a7d2e84c5ac3536340c9ea7b283f983f9b2
Veaceslav Chirilov <veaceslav.chirilov@amdaris.com>
2023-12-04 15:07:44 +0200

    Removed unused methods

--- Excel.Loader.WebApp/Helpers/AttributeHelper.cs
+++ Excel.Loader.WebApp/Helpers/AttributeHelper.cs
@@ -10,21 +10,5 @@ namespace Excel.Loader.WebApp.Helpers
               .GetProperties()
               .OrderBy(p => ((OrderAttribute)p.GetCustomAttribute(typeof(OrderAttribute), false)).Key);
         }
-
-        public static IOrderedEnumerable<PropertyInfo> GetSortedProperties(object obj)
-        {
-            return obj.GetType()
-              .GetProperties()
-              .OrderBy(p => ((OrderAttribute)p.GetCustomAttribute(typeof(OrderAttribute), false)).Key);
-        }
-
-        public static PropertyInfo GetPropertyByKey<T>(int orderKey) => typeof(T).GetProperties()
-            .Where(p => ((OrderAttribute)p.GetCustomAttribute(typeof(OrderAttribute), false)).Key.Equals(orderKey))
-            .FirstOrDefault();
-
-        public static PropertyInfo GetPropertyByKey(Type type, int orderKey) => type.GetProperties()
-            .Where(p => ((OrderAttribute)p.GetCustomAttribute(typeof(OrderAttribute), false)).Key.Equals(orderKey))
-            .FirstOrDefault();
-
     }
 }
